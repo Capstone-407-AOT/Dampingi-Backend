6d73573b1217b557f6f76f4c12199d4c276812a9
Sandi Bhirama <43607241+sanbhir14@users.noreply.github.com>
2021-05-29 14:02:49 +0700

    finishing panic button service

--- panic/views.py
+++ panic/views.py
@@ -3,6 +3,7 @@ from rest_framework import permissions, viewsets,  views, status
 from rest_framework.response import Response
 from .serializers import PanicSerializer
 from .models import Panic
+from jwtauth.models import Emergency
 from django.conf import settings
 from twilio.rest import Client
 from django.core.mail import send_mail
@@ -11,21 +12,25 @@ class PanicView(viewsets.ModelViewSet):
     # queryset = Panic.objects.all()
     serializer_class = PanicSerializer
 
-    def get_queryset(self):
+    def create(self,request, *args, **kwargs):
         user = self.request.user
-       
-        #Twilio Send SMS
-        message_to_broadcast = ("Server Test "
-                                                "yet? Grab it here: https://www.twilio.com/quest")
-        client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-        client.messages.create(to="+6287884357149",
-                                   from_="+14437207598",
-                                   body=message_to_broadcast)
-
-        #SMTP Send Mail
+        emergency = Emergency.objects.filter(profile = user.profile.id)
+        print(user.profile)
+        print(emergency)
         subject = 'Test Server'
         message = 'Dampingi Backend Test Serve : Tolong hubungi sayaa'
-        recepient = 'sbhirama@gmail.com'
-        send_mail(subject, 
-            message, settings.EMAIL_HOST_USER, [recepient])
-        return Panic.objects.filter(profile=user.profile)
+        for ec in emergency:
+            recepient = ec.email
+            send_mail(subject, message, settings.EMAIL_HOST_USER, [recepient])
+
+        #Twilio Send SMS
+        # message_to_broadcast = ("Server Test "
+        #                                         "yet? Grab it here: https://www.twilio.com/quest")
+        # client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+        # client.messages.create(to="+6287884357149",
+        #                            from_="+14437207598",
+        #                            body=message_to_broadcast)
+
+        #SMTP Send Mail
+        
+        return Response("Pesan Berhasil Dikirim")
